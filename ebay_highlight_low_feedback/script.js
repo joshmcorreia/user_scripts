8e0841eecc71130b77a33981dc51450a1948d47b
Josh Correia <joshmcorreia@gmail.com>
2025-12-06 20:12:29 -0800

    Update highlight low feedback to use doc-start and observers

--- ebay_highlight_low_feedback/script.js
+++ ebay_highlight_low_feedback/script.js
@@ -3,19 +3,20 @@
 // @description  Easily highlight sellers with low feedback.
 // @author       joshmcorreia
 // @match        https://www.ebay.com/itm/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        none
 // @license      MIT
-// @version      0.0.1
+// @version      1.0.0
 // @namespace    https://greasyfork.org/users/1220845
 // @noframes
 // ==/UserScript==
 
 // GitHub repo can be found at https://github.com/joshmcorreia/user_scripts
 
+const feedback_selector = ".x-sellercard-atf_main";
+
 var RED_FEEDBACK_THRESHHOLD=0;
 var ORANGE_FEEDBACK_THRESHHOLD=10;
-
 
 function get_feedback_number() {
 	let feedback_number_string = document.querySelector(".x-sellercard-atf .x-sellercard-atf__info__about-seller .x-sellercard-atf__about-seller .ux-textspans")?.textContent;
@@ -24,17 +25,26 @@ function get_feedback_number() {
 	return feedback_number;
 }
 
-
 function highlight_seller_with_color(color) {
 	let seller_card_div = document.querySelector(".x-sellercard-atf_main");
 	seller_card_div.style = `background: ${color}`;
 }
 
-
-let feedback_number = get_feedback_number();
-if (feedback_number == RED_FEEDBACK_THRESHHOLD) {
-	highlight_seller_with_color("#d11d1d");
+function check_feedback() {
+	let feedback_number = get_feedback_number();
+	if (feedback_number <= RED_FEEDBACK_THRESHHOLD) {
+		highlight_seller_with_color("#d11d1d");
+	}
+	else if (feedback_number <= ORANGE_FEEDBACK_THRESHHOLD) {
+		highlight_seller_with_color("#F28C28");
+	}
 }
-else if (feedback_number <= ORANGE_FEEDBACK_THRESHHOLD) {
-	highlight_seller_with_color("#F28C28");
+
+(new MutationObserver(observe_feedback_element)).observe(document, {childList: true, subtree: true});
+
+function observe_feedback_element(changes, observer) {
+	if(document.querySelector(feedback_selector)) {
+		observer.disconnect();
+		check_feedback();
+	}
 }

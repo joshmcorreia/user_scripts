7668e6e545ec004e7fb4673d7437ac63280c4105
Josh Correia <joshmcorreia@gmail.com>
2025-12-06 20:18:45 -0800

    Hide finance option at document start

--- ebay_hide_finance_option_below_price/script.js
+++ ebay_hide_finance_option_below_price/script.js
@@ -2,8 +2,9 @@
 // @name        eBay Hide Finance Options Below Price
 // @namespace   Violentmonkey Scripts
 // @match       https://www.ebay.com/itm/*
+// @run-at      document-start
 // @grant       none
-// @version     1.0.0
+// @version     1.0.1
 // @author      joshmcorreia
 // @license     MIT
 // @description Hides the Finance options below the price.
@@ -19,14 +20,19 @@ function remove_finance_info_under_price() {
 	console.log("eBay Hide Finance Options - User Script - Removed finance info under price.")
 }
 
-
 if (window.location.href.indexOf("ebay.com/itm/") != -1) {
 	(new MutationObserver(observe_finance_info_under_price)).observe(document, {childList: true, subtree: true});
 }
 
+var num_times_triggered = 0;
 function observe_finance_info_under_price(changes, observer) {
 	if(document.querySelector(financing_details_selector)) {
-		observer.disconnect();
+		num_times_triggered += 1;
 		remove_finance_info_under_price();
+		// The finance info comes up twice for some reason, so we need to remove both
+		// before disconnecting the observer.
+		if (num_times_triggered == 2) {
+			observer.disconnect();
+		}
 	}
 }

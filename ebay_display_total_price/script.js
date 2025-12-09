33791c1621b612269222561435821a394889919f
Josh Correia <joshmcorreia@gmail.com>
2025-12-08 19:06:34 -0800

    Working BIN

--- ebay_display_total_price/script.js
+++ ebay_display_total_price/script.js
@@ -17,6 +17,7 @@
 // TODO: Support active bids breaking the price of the item.
 
 const price_element_selector = ".x-price-section";
+const BIN_price_element_selector = ".x-bin-price"
 
 /**
  * @param {String} input_string
@@ -104,21 +105,6 @@ function get_total_price(item_price, shipping_price) {
 }
 
 /**
- * @param {String} total_bid_price
- * @return {}
- */
-function add_total_bid_price_to_page(total_bid_price) {
-	let total_bid_price_div = document.createElement('div');
-	total_bid_price_div.style = "color:DodgerBlue";
-	total_bid_price_div.className = "x-price-primary";
-	total_bid_price_div.textContent = `US $${total_bid_price}`;
-	document.querySelector(".x-price-section").prepend(document.createElement('br'));
-	document.querySelector(".x-price-section").prepend(total_bid_price_div);
-	console.log("Added total bid price to page.")
-	return;
-}
-
-/**
  * @param {String} total_BIN_price
  * @return {}
  */
@@ -127,56 +113,59 @@ function add_total_BIN_price_to_page(total_BIN_price) {
 	total_BIN_price_div.style = "color:green";
 	total_BIN_price_div.className = "x-price-primary";
 	total_BIN_price_div.id = "total_BIN_price";
-	total_BIN_price_div.textContent = `US $${total_BIN_price}`;
-	let price_div = document.querySelector(".x-bin-price__content");
-	price_div.prepend(document.createElement('br'));
-	price_div.prepend(total_BIN_price_div);
+	total_BIN_price_div.innerHTML = `<span class="ux-textspans">US $${total_BIN_price}</span>`
+	let BIN_price_div = document.querySelector(".x-bin-price__content");
+	BIN_price_div.prepend(document.createElement('br'));
+	BIN_price_div.prepend(total_BIN_price_div);
 	console.log("Added total BIN price to page.")
-	// alert();
 	return;
 }
 
-/**
- * @returns {}
- */
-function add_total_price_to_page() {
-	let primary_BIN_price = get_primary_BIN_price();
-	let primary_bid_price = get_primary_bid_price();
-	let shipping_price = get_shipping_price();
-
-	let total_BIN_price = get_total_price(primary_BIN_price, shipping_price);
-	let total_bid_price = get_total_price(primary_bid_price, shipping_price);
-
-	if (total_BIN_price !== undefined) {
-		add_total_BIN_price_to_page(total_BIN_price);
-	}
-	if (total_bid_price !== undefined) {
-		add_total_bid_price_to_page(total_bid_price);
-	}
-}
-
-(new MutationObserver(observe_price_loading)).observe(document, {childList: true, subtree: true});
-
 // Found at https://www.reddit.com/r/GreaseMonkey/comments/undlw2/need_to_monitor_changes_to_a_specific_element_on/
-const observeDOM = (fn, e = document.documentElement, config = { attributes: 1, childList: 1, subtree: 1 }) => {
+const observeDOM = (fn, e = document.documentElement, config = { attributes: true, childList: true, subtree: true }) => {
+	console.log("Created observer");
 	const observer = new MutationObserver(fn);
 	observer.observe(e, config);
 	return () => observer.disconnect();
 };
 
-function readd_elements() {
-	add_total_price_to_page();
-	console.log("Re-added");
+let num_BIN_rewrites = 0;
+let max_BIN_rewrites = 3;
+function rewrite_BIN_section(total_BIN_price_html_after_edit) {
+	num_BIN_rewrites += 1;
+	if (num_BIN_rewrites > max_BIN_rewrites) {
+		console.log("Max num BIN rewrites exceeded!")
+		return;
+	}
+
+	let before_edit_BIN_price_html = document.querySelector(BIN_price_element_selector).innerHTML;
+	if (before_edit_BIN_price_html !== total_BIN_price_html_after_edit) {
+		document.querySelector(BIN_price_element_selector).innerHTML = total_BIN_price_html_after_edit;
+		console.log(`Rewrote the BIN section\n\n${before_edit_BIN_price_html}\n\n${total_BIN_price_html_after_edit}`)
+	}
 }
 
+let total_BIN_price_html_after_edit = "";
 function observe_price_loading(changes, observer) {
 	if(document.querySelector(price_element_selector)) {
 		observer.disconnect();
-		add_total_price_to_page();
 
-		// eBay has some code in place that re-writes the price multiple times for some reason. I
-		// originally thought it was to prevent scripts from editing the price on the page but
-		// they're re-written regardless of if the element is edited or not.
-		observeDOM(() => readd_elements(), document.querySelector("#total_BIN_price"));
+		let primary_BIN_price = get_primary_BIN_price();
+		let shipping_price = get_shipping_price();
+		let total_BIN_price = get_total_price(primary_BIN_price, shipping_price);
+		if (total_BIN_price !== undefined) {
+			add_total_BIN_price_to_page(total_BIN_price);
+
+			// Now that we edited the BIN price, we need to save a copy of the HTML so we
+			// can overwrite eBay changing the page
+			total_BIN_price_html_after_edit = document.querySelector(BIN_price_element_selector).innerHTML;
+
+			// eBay has some code in place that re-writes the price multiple times for some reason. I
+			// originally thought it was to prevent scripts from editing the price on the page but
+			// they're re-written regardless of if the element is edited or not.
+			observeDOM(() => rewrite_BIN_section(total_BIN_price_html_after_edit), document.querySelector(BIN_price_element_selector));
+		}
 	}
 }
+
+(new MutationObserver(observe_price_loading)).observe(document, {childList: true, subtree: true});

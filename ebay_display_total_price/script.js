56ba4aa04575f59b68c92dc9d1198a58c343cf7c
Josh Correia <joshmcorreia@gmail.com>
2025-12-08 17:48:59 -0800

    WIP

--- ebay_display_total_price/script.js
+++ ebay_display_total_price/script.js
@@ -3,12 +3,11 @@
 // @description  Easily see the total price of your order including shipping.
 // @author       joshmcorreia
 // @match        https://www.ebay.com/itm/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        none
 // @license      MIT
-// @version      0.0.1
+// @version      1.0.0
 // @namespace    https://greasyfork.org/users/1220845
-// @noframes
 // ==/UserScript==
 
 // GitHub repo can be found at https://github.com/joshmcorreia/user_scripts
@@ -16,6 +15,8 @@
 // NOTE: Does not support "Local pickup only" by design. If you want to pick items up locally you probably shouldn't be using eBay.
 
 // TODO: Support active bids breaking the price of the item.
+
+const price_element_selector = ".x-price-section";
 
 /**
  * @param {String} input_string
@@ -113,6 +114,7 @@ function add_total_bid_price_to_page(total_bid_price) {
 	total_bid_price_div.textContent = `US $${total_bid_price}`;
 	document.querySelector(".x-price-section").prepend(document.createElement('br'));
 	document.querySelector(".x-price-section").prepend(total_bid_price_div);
+	console.log("Added total bid price to page.")
 	return;
 }
 
@@ -124,9 +126,13 @@ function add_total_BIN_price_to_page(total_BIN_price) {
 	let total_BIN_price_div = document.createElement('div');
 	total_BIN_price_div.style = "color:green";
 	total_BIN_price_div.className = "x-price-primary";
+	total_BIN_price_div.id = "total_BIN_price";
 	total_BIN_price_div.textContent = `US $${total_BIN_price}`;
-	document.querySelector(".x-bin-price__content").prepend(document.createElement('br'));
-	document.querySelector(".x-bin-price__content").prepend(total_BIN_price_div);
+	let price_div = document.querySelector(".x-bin-price__content");
+	price_div.prepend(document.createElement('br'));
+	price_div.prepend(total_BIN_price_div);
+	console.log("Added total BIN price to page.")
+	// alert();
 	return;
 }
 
@@ -134,19 +140,43 @@ function add_total_BIN_price_to_page(total_BIN_price) {
  * @returns {}
  */
 function add_total_price_to_page() {
-	let primary_BIN_price = get_primary_BIN_price()
-	let primary_bid_price = get_primary_bid_price()
-	let shipping_price = get_shipping_price()
+	let primary_BIN_price = get_primary_BIN_price();
+	let primary_bid_price = get_primary_bid_price();
+	let shipping_price = get_shipping_price();
 
 	let total_BIN_price = get_total_price(primary_BIN_price, shipping_price);
 	let total_bid_price = get_total_price(primary_bid_price, shipping_price);
 
 	if (total_BIN_price !== undefined) {
-		add_total_BIN_price_to_page(total_BIN_price)
+		add_total_BIN_price_to_page(total_BIN_price);
 	}
 	if (total_bid_price !== undefined) {
-		add_total_bid_price_to_page(total_bid_price)
+		add_total_bid_price_to_page(total_bid_price);
 	}
 }
 
-add_total_price_to_page();
+(new MutationObserver(observe_price_loading)).observe(document, {childList: true, subtree: true});
+
+// Found at https://www.reddit.com/r/GreaseMonkey/comments/undlw2/need_to_monitor_changes_to_a_specific_element_on/
+const observeDOM = (fn, e = document.documentElement, config = { attributes: 1, childList: 1, subtree: 1 }) => {
+	const observer = new MutationObserver(fn);
+	observer.observe(e, config);
+	return () => observer.disconnect();
+};
+
+function readd_elements() {
+	add_total_price_to_page();
+	console.log("Re-added");
+}
+
+function observe_price_loading(changes, observer) {
+	if(document.querySelector(price_element_selector)) {
+		observer.disconnect();
+		add_total_price_to_page();
+
+		// eBay has some code in place that re-writes the price multiple times for some reason. I
+		// originally thought it was to prevent scripts from editing the price on the page but
+		// they're re-written regardless of if the element is edited or not.
+		observeDOM(() => readd_elements(), document.querySelector("#total_BIN_price"));
+	}
+}
